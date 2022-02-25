827bcd252d34c3a86fbbf3db8033de4c4b334ed4
jchayanan <chayanan_jame@hotmail.com>
2022-02-26 06:52:21 +0700

    Updates

---
+++ precache-manifest.1e7ec0eaea0b0f74fc01fbb0c4b3df6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6bd9423e4827f4a7bcf7c7f0afdb97",
+    "url": "/dapp_project/index.html"
+  },
+  {
+    "revision": "a87eea82667c43313803",
+    "url": "/dapp_project/static/css/2.6a23f8e0.chunk.css"
+  },
+  {
+    "revision": "3daa215a63a7fb7ec74b",
+    "url": "/dapp_project/static/css/main.50667174.chunk.css"
+  },
+  {
+    "revision": "a87eea82667c43313803",
+    "url": "/dapp_project/static/js/2.6c37d002.chunk.js"
+  },
+  {
+    "revision": "3daa215a63a7fb7ec74b",
+    "url": "/dapp_project/static/js/main.0a7d9b41.chunk.js"
+  },
+  {
+    "revision": "d43631d184521748b6b0",
+    "url": "/dapp_project/static/js/runtime-main.ca742af1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dapp_project/precache-manifest.e1e33c216fa2c0cd438b02363ef4bafb.js"
+  "/dapp_project/precache-manifest.1e7ec0eaea0b0f74fc01fbb0c4b3df6d.js"
 );
 
 self.addEventListener('message', (event) => {
